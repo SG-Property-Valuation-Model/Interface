46e900ab17a2cedd7440171c1afff5f41b09f6c6
Hui Yee <hui.yee.1998@gmail.com>
2021-03-25 17:48:28 +0800

    added listing class, added model files, edited layout.py

--- listing.py
+++ listing.py
@@ -7,10 +7,10 @@ nearest train line set as BLUE for now
 import pandas as pd
 import numpy as np
 import geopandas as gp
-from school import nearest_sch
+from amenities import nearest_sch, nearest_police_centre
 from location import postal_search, area_region
 import joblib
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
+import xgboost
 
 class Listing:
     def __init__(self, postal, property_type, floor_num, floor_area, remaining_lease):
@@ -49,6 +49,16 @@ class Listing:
         planning_region = area_region(self.postal, area_centroid)[1]
         return planning_region
 
+    def get_police_centre(self, police_centre_gdf):
+        geom = self.get_geom()
+        police_centre = nearest_police_centre(geom, police_centre_gdf)
+        return police_centre
+
+    def get_centre_avg_cases(self, police_centre_gdf, avg_cases_by_npc):
+        police_centre = self.get_police_centre(police_centre_gdf)
+        avg_case = avg_cases_by_npc[avg_cases_by_npc['Police Centre'] == police_centre]['Average Cases Per Year'].values[0]
+        return avg_case
+
     # not done
     def train_dist(self):
         return 0
@@ -59,79 +69,80 @@ class Listing:
 
     def sch_dist(self, sch_gdf):
         geom = self.get_geom()
-        nearest_dist = nearest_sch(geom, sch_gdf, 2000)
+        nearest_dist = nearest_sch(geom, sch_gdf)
         return nearest_dist
 
     # create dataframe containing property details to be used for prediction
-    def convert_to_df(self, main_df_col, area_centroids, sch_gdf): # parse in list of training df col because predict df needs to be in same order
-
-        col_lowercase = [x.lower() for x in main_df_col]
+    def convert_to_df(self, main_df_col, area_centroids, sch_gdf, police_centre_gdf, avg_cases_by_npc): # parse in list of training df col because predict df needs to be in same order
         # Create dataframe for property for prediction
-        df = pd.DataFrame(columns=col_lowercase, index=range(1))
-        df['area (sqm)'] = self.floor_area
-        df['floor number'] = self.floor_num
-        df['ppi'] = 153.3 #KIV
-        df['average cases per year'] = 0 #KIV???
-        df['nearest primary school'] = self.sch_dist(sch_gdf)
+        df = pd.DataFrame(columns=main_df_col, index=range(1))
+        df['Area (SQM)'] = self.floor_area
+        df['Floor Number'] = self.floor_num
+        df['PPI'] = 153.3 #KIV
+        df['Average Cases Per Year'] = self.get_centre_avg_cases(police_centre_gdf, avg_cases_by_npc)
+        df['Nearest Primary School'] = self.sch_dist(sch_gdf)
         df['nearest_station_distance'] = self.train_dist()
-        df['remaining lease'] = self.remaining_lease
+        df['Remaining Lease'] = self.remaining_lease
 
         # if the column(s) is not the base dummy column that got dropped
-        if self.get_planning_area(area_centroids).lower() in col_lowercase:
-            df[self.get_planning_area(area_centroids).lower()] = 1
-        if self.property_type.lower() in col_lowercase:
-            df[self.property_type.lower()] = 1
-        if self.get_mrt_line().lower() in col_lowercase:
-            df[self.get_mrt_line().lower()] = 1
+        if self.get_planning_area(area_centroids) in main_df_col:
+            df[self.get_planning_area(area_centroids)] = 1
+        if self.property_type in main_df_col:
+            df[self.property_type] = 1
+        if self.get_mrt_line() in main_df_col:
+            df[self.get_mrt_line()] = 1
 
         # for remaining one-hot encoded columns that are Nan, replace with 0
         df = df.fillna(0)
         return df
 
-    def pred_psm(self, path, main_df_col, area_centroids, sch_gdf):
-        property_df = self.convert_to_df(main_df_col, area_centroids, sch_gdf)
+    def pred_psm(self, path, main_df_col, area_centroids, sch_gdf, police_centre_gdf, avg_cases_by_npc):
+        property_df = self.convert_to_df(main_df_col, area_centroids, sch_gdf, police_centre_gdf, avg_cases_by_npc)
         s_scaler = joblib.load(path + 'standard_scaler.bin')
         mm_scaler = joblib.load(path + 'mm_scaler.bin')
-        standardScale_vars = ['area (sqm)',
-                              'floor number',
-                              'ppi',
-                              'average cases per year',
-                              'nearest primary school',
+        standardScale_vars = ['Area (SQM)',
+                              'Floor Number',
+                              'PPI',
+                              'Average Cases Per Year',
+                              'Nearest Primary School',
                               'nearest_station_distance']
-        minMax_vars = ['remaining lease']
+        minMax_vars = ['Remaining Lease']
         s_scaled = pd.DataFrame(s_scaler.transform(property_df.loc[:, standardScale_vars].copy()),
                                 columns=standardScale_vars)
         mm_scaled = pd.DataFrame(mm_scaler.transform(property_df.loc[:, minMax_vars].copy()), columns=minMax_vars)
 
         property_df_scaled = pd.concat([s_scaled,
                         mm_scaled,
-                        property_df.loc[:, 'ang mo kio':'executive condominium'].copy()], axis=1)
+                        property_df.loc[:, 'Ang Mo Kio':'Executive Condominium'].copy()], axis=1)
         model = joblib.load(path + 'model_test.pkl')
         # Use the loaded model to make predictions
         prediction = model.predict(property_df_scaled)[0]
         return prediction
 
 
-    def pred_price(self, path, main_df_col, area_centroids, sch_gdf):
+    def pred_price(self, path, main_df_col, area_centroids, sch_gdf, police_centre_gdf, avg_cases_by_npc):
         '''
         :param path: takes in path where model weights and scalers are stored
         :param main_df_col: list of training dataset column names so that prediction df tallies
         :param area_centroids: to get the planning area property is in
         :param sch_gdf: to get nearest school distance
+        :param police_centre_gdf: to get nearest police centre
+        :param avg_cases_by_npc: to get avg crime cases per year for nearest police centre
         :return: predicted price of unit and predicted price per sqm
         '''
-        predicted_psm = self.pred_psm(path, main_df_col, area_centroids, sch_gdf)
-        print('floor:', self.floor_area)
-        print(type(self.floor_area))
-        print('predict:', predicted_psm)
+        predicted_psm = self.pred_psm(path, main_df_col, area_centroids, sch_gdf, police_centre_gdf, avg_cases_by_npc)
         unit_price = self.floor_area * predicted_psm
         return unit_price, predicted_psm
 
 '''
-property = Listing('098656', 'Condominium', 6, 99, 35)
-sch = pd.read_csv('C:/Users/User/Desktop/NOTES/Notes_Y4S2/BT4222/HDB Project/Final Merge of Data/primary_sch_gdf.csv')
-area_df = pd.read_csv('C:/Users/User/Desktop/NOTES/Notes_Y4S2/BT4222/HDB Project/Interface/area_centroid.csv')
-modelling = pd.read_csv('C:/Users/User/Desktop/NOTES/Notes_Y4S2/BT4222/HDB Project/Modelling/modelling_dataset.csv')
+# TESTING
+#Input postal code, property type, floor num, sqm, remaining lease
+property = Listing('659921', 'Condominium', 6, 99, 70)
+sch = pd.read_csv('datasets/primary_sch_gdf.csv')
+area_df = pd.read_csv('datasets/area_centroid.csv')
+modelling = pd.read_csv('datasets/modelling_dataset.csv')
+police_centre = pd.read_csv('datasets/police_centre_gdf.csv')
+avg_cases = pd.read_csv('datasets/average_cases_by_npc.csv')
 cols = list(modelling.columns)
 cols.remove('Unit Price ($ PSM)')
 print(cols)
@@ -141,6 +152,8 @@ print(property.get_geom())
 print(property.get_planning_area(area_df))
 print(property.get_planning_region(area_df))
 print(property.sch_dist(sch))
-print(property.convert_to_df(cols, area_df, sch))
-print(property.pred_price('C:/Users/User/Desktop/NOTES/Notes_Y4S2/BT4222/HDB Project/Modelling/', cols, area_df, sch))
+print(property.get_police_centre(police_centre))
+print(property.get_centre_avg_cases(police_centre, avg_cases))
+print(property.convert_to_df(cols, area_df, sch, police_centre, avg_cases))
+print(property.pred_price('modelling/', cols, area_df, sch, police_centre, avg_cases))
 '''
--- modelling/model_test.py
+++ modelling/model_test.py
@@ -1,83 +1,91 @@
 '''
 To test model and scaler saving
-Ridge model is a placeholder for now; needs to be replaced by final model
+XGBoost model for now; needs to be replaced by final model
 '''
 
 import pandas as pd
 import numpy as np
 import os
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import Ridge
+from xgboost import XGBRegressor
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 import joblib
 
-df = pd.read_csv('../datasets/final_dataset.csv')
-df['Sale Date'] = pd.to_datetime(df['Sale Date'])
-df = pd.concat([df, pd.get_dummies(df.pop('Property Type'))], axis=1)
-#df['Time Period'] = df['Sale Date'].apply(lambda x: (x.year - df['Sale Date'][0].year)*12 + (x.month - df['Sale Date'][0].month))
-df.drop(columns=['BLK_NO',
-                 'BUILDING',
-                 'POSTAL',
-                 'Planning Area',
-                 'Planning Region',
-                 'Postal Code',
-                 'ROAD_NAME',
-                 'Sale Date',
-                 'Transacted Price ($)',
-                 'Police Centre',
-                 'num_stations_1km',
-                 'Number of Primary Schools'], inplace=True)
-df.drop_duplicates(inplace=True)
-df.reset_index(drop=True, inplace=True)
-df.drop(columns=['Age Sold',
-                 'Woodlands',
-                 'Condominium'], inplace=True)
+df = pd.read_csv('../datasets/modelling_dataset.csv')
 
-print(df.shape)
-print(df.head(3))
-
-X = df.drop(columns = ['Unit Price ($ PSM)'])
+X = df.drop(["Unit Price ($ PSM)"], axis=1)
 y = df['Unit Price ($ PSM)']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, shuffle = False, random_state = None)
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, shuffle=True)
 
+all_features = list(X_train.columns)
+print(all_features)
 standardScale_vars = ['Area (SQM)',
                       'Floor Number',
                       'PPI',
                       'Average Cases Per Year',
                       'Nearest Primary School',
                       'nearest_station_distance']
+
 minMax_vars = ['Remaining Lease']
 
+remaining_features = [x for x in all_features if x not in standardScale_vars and x not in minMax_vars]
 s_scaler = StandardScaler()
 mm_scaler = MinMaxScaler()
 
-X_train_std = s_scaler.fit_transform(X_train.loc[:, standardScale_vars].copy())
-X_train_mm = mm_scaler.fit_transform(X_train.loc[:, minMax_vars].copy())
-X_test_std = s_scaler.transform(X_test.loc[:, standardScale_vars].copy())
-X_test_mm = mm_scaler.transform(X_test.loc[:, minMax_vars].copy())
+s_scaled = pd.DataFrame(s_scaler.fit_transform(X_train.loc[:, standardScale_vars].copy()), columns=standardScale_vars, index=X_train.index)
+mm_scaled = pd.DataFrame(mm_scaler.fit_transform(X_train.loc[:, minMax_vars].copy()), columns=minMax_vars, index=X_train.index)
 
-X_train_df = pd.concat([pd.DataFrame(X_train_std, columns=standardScale_vars),
-                       pd.DataFrame(X_train_mm, columns = minMax_vars),
-                       X_train.loc[:, 'Ang Mo Kio':'Executive Condominium'].copy()], axis = 1)
-X_test_df = pd.concat([pd.DataFrame(X_test_std, columns=standardScale_vars),
-                       pd.DataFrame(X_test_mm, columns = minMax_vars),
-                       X_test.loc[:, 'Ang Mo Kio':'Executive Condominium'].copy()], axis = 1)
+X_train = pd.concat([s_scaled,
+                     mm_scaled,
+                     X_train.loc[:, remaining_features].copy()], axis=1)
 
-regressor = Ridge(alpha = 1.10, fit_intercept = True)
-regressor.fit(X_train_df, y_train)
+s_scaled_test = pd.DataFrame(s_scaler.transform(X_test.loc[:, standardScale_vars].copy()), columns=standardScale_vars, index=X_test.index)
+mm_scaled_test = pd.DataFrame(mm_scaler.transform(X_test.loc[:, minMax_vars].copy()), columns=minMax_vars, index=X_test.index)
 
-joblib.dump(regressor, 'model_test.pkl')
+X_test = pd.concat([s_scaled_test,
+                     mm_scaled_test,
+                     X_test.loc[:, remaining_features].copy()], axis=1)
+
+
+# Split training set into train and eval sets
+X_train, X_eval, y_train, y_eval = train_test_split(X_train, y_train, test_size=0.2, random_state=42, shuffle=True)
+
+# Best model parameters
+model = XGBRegressor(colsample_bytree=0.7,
+                     gamma=5,
+                     learning_rate=0.07,
+                     max_depth=7,
+                     min_child_weight=5,
+                     n_estimators=4675,
+                     reg_linear=0.6,
+                     reg_lambda=0.4,
+                     subsample=0.7,
+                     seed=42)
+
+
+model.fit(X_train, y_train, eval_set=[(X_eval, y_eval)], early_stopping_rounds=50)
+
+joblib.dump(model, 'model_test.pkl')
 joblib.dump(s_scaler, 'standard_scaler.bin')
 joblib.dump(mm_scaler, 'mm_scaler.bin')
-'''
 
 # Load the model and scalers from the file
-model_from_joblib = joblib.load('C:/Users/User/Desktop/NOTES/Notes_Y4S2/BT4222/HDB Project/Modelling/model_test.pkl')
-sc_ = joblib.load('C:/Users/User/Desktop/NOTES/Notes_Y4S2/BT4222/HDB Project/Modelling/standard_scaler.bin')
-mm_ = joblib.load('C:/Users/User/Desktop/NOTES/Notes_Y4S2/BT4222/HDB Project/Modelling/mm_scaler.bin')
+model_from_joblib = joblib.load('model_test.pkl')
+s_scaler = joblib.load('standard_scaler.bin')
+mm_scaler = joblib.load('mm_scaler.bin')
 
+s_scaled = pd.DataFrame(s_scaler.transform(X_test[:1].loc[:, standardScale_vars].copy()),
+                        columns=standardScale_vars)
+mm_scaled = pd.DataFrame(mm_scaler.transform(X_test[:1].loc[:, minMax_vars].copy()), columns=minMax_vars)
 
+property_df_scaled = pd.concat([s_scaled,
+                                mm_scaled,
+                                X_test[:1].loc[:, 'Ang Mo Kio':'Executive Condominium'].copy()], axis=1)
+
+prediction = model_from_joblib.predict(property_df_scaled)[0]
+
+'''
 s_scaled = pd.DataFrame(s_scaler.fit_transform(df.loc[:, standardScale_vars].copy()), columns=standardScale_vars)
 mm_scaled = pd.DataFrame(mm_scaler.fit_transform(df.loc[:, minMax_vars].copy()), columns=minMax_vars)
 
